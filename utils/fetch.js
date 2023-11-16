6106e3dd5bd60709eeb6893eb203794f21e7a318
AxelothLeohryn <alejandro97ss@gmail.com>
2023-11-16 23:40:52 +0100

    tables, api_key, routes fixed

--- utils/fetch.js
+++ utils/fetch.js
@@ -3,39 +3,39 @@
 
 
 const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        `Bearer ${process.env.API_KEY}`,
-    },
-  };
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      `Bearer ${process.env.API_KEY}`,
+  },
+};
 //Funciones que tengan que ver con hacer un fetch:
 async function searchFilms(query) {
-    //Llamada a la API para buscar, devolvemos array de objetos
-    //   try {
-    //     return await fetch(............. url busqueda movies)
-    //   } catch (error) {
-    //     return {
-    //       message: error.message,
-    //     };
-    //   }
-  }
-  
-  async function searchFilmDetails(id) {
-    //El id lo vamos a obtener del objeto en el que clickeamos para "ver detalles"
-    //Llamada a la API para buscar detalles de una película, devolvemos objeto con detalles
-    //   try {
-    //     return await fetch(............ url busqueda por id)
-    //   } catch (error) {
-    //     return {
-    //       message: error.message,
-    //     };
-    //   }
-  }
-  
-  //Exportaciones
-  module.exports = {
-    searchFilms,
-    searchFilmDetails,
-  };
+  //Llamada a la API para buscar, devolvemos array de objetos
+  //   try {
+  //     return await fetch(............. url busqueda movies)
+  //   } catch (error) {
+  //     return {
+  //       message: error.message,
+  //     };
+  //   }
+}
+
+async function searchFilmDetails(id) {
+  //El id lo vamos a obtener del objeto en el que clickeamos para "ver detalles"
+  //Llamada a la API para buscar detalles de una película, devolvemos objeto con detalles
+  //   try {
+  //     return await fetch(............ url busqueda por id)
+  //   } catch (error) {
+  //     return {
+  //       message: error.message,
+  //     };
+  //   }
+}
+
+//Exportaciones
+module.exports = {
+  searchFilms,
+  searchFilmDetails,
+};
