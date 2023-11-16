dcf20a7b3144ca1a6489aa1595e0690f4b3c6c87
AxelothLeohryn <alejandro97ss@gmail.com>
2023-11-16 12:55:22 +0100

    search router

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,12 +1,29 @@
 //Importaciones
-
-
+const fetch = require("../utils/fetch")
 //Funciones (req,res)
+const searchMovieDetails = async (req, res) => {
+  async (req, res) => {
+    const title = req.params.title;
+    console.log(title);
+    //-----------------Descomentar cuando se cree la funcion de searchFilmByTitle-----------------
+    //   const movieDetails = await fetch.searchFilmByTitle(title);
+    //   if (movieDetails.total_results > 0) {
+    //     console.log(movieDetails);
+    //     res.render("film", { movieDetails });
+    //   } else {
+    //     res.render("error");
+    //   }
 
-
-
+    //De momento como prueba:
+    if (title === "pruebaerror") {
+      res.render("search-error");
+    } else {
+      res.render("search-details", { title });
+    }
+  };
+};
 
 //Exportaciones
-// module.exports = {
-    
-// }
+module.exports = {
+    searchMovieDetails
+}
