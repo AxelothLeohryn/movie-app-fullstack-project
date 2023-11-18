5648aa0419f67fa75b37959bf99373b94e506ae5
ElenaDRFS <elenafullstack@gmail.com>
2023-11-18 11:00:19 +0100

    cleaned index

--- index.js
+++ index.js
@@ -48,34 +48,6 @@ async function createMovie(id,title,description,image,year) {
 app.set("view engine", "pug");
 app.set("views", "./views");
 
-// coneción a atlas (copiado de atlas)
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://elena_del_rio:20404020@movie-app.6m5tlpe.mongodb.net/?retryWrites=true&w=majority";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
 
 //Rutas
 app.use("/", viewsRoutes);
