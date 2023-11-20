42e1d3a6288f1d79ecb77b472a5f79b81d16f059
alicia3194 <aliciacortinez@gmail.com>
2023-11-20 16:05:56 +0100

    form admi and user

--- routes/views.routes.js
+++ routes/views.routes.js
@@ -9,12 +9,17 @@ const moviesControllers = require("../controllers/movies.controller");
 const inicioController = require("../controllers/inicio.controller");
 const dashboardController = require("../controllers/dashboard.controller");
 
-
 router.get("/", inicioController.getInicio);
-router.get("/dashboard", isAuthenticated, checkToken, dashboardController.getDashboard);
+router.get(
+  "/dashboard",
+  isAuthenticated,
+  checkToken,
+  dashboardController.getDashboard
+);
 router.get("/search/:id?", searchController.searchView); //search page if no id, search details if id
 router.get("/movies", moviesControllers.getMovies);
-
+router.get("/createMovie", moviesControllers.createMoviesForm);
+router.get("/editMovie/:id", moviesControllers.editMoviesForm);
 
 //Ruta temporal para probar view del navbar
 router.get("/navbar", (req, res) => {
