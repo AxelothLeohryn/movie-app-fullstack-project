7fc83720210bbd3ded769cf9fcfb0974e63d4492
AxelothLeohryn <alejandro97ss@gmail.com>
2023-11-23 10:53:10 +0100

    console.log createmovie

--- controllers/bbdd.controller.js
+++ controllers/bbdd.controller.js
@@ -13,9 +13,9 @@ const getAllMovies = async (req, res) => {
 
 const createMovie = async (req, res) => {
   try {
-    console.log(req.body);
     const newMovie = new Movie(req.body);
     const savedMovie = await newMovie.save();
+    console.log(savedMovie);
     res.json(savedMovie);
   } catch (error) {
     res.status(500).json(error.message);
